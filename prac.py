7a62b81ae09e9024543945429f4e91976e08430b
phani497 <phaniiim9@gmail.com>
2025-09-21 10:34:50 +0530

    this is first commit

---
+++ prac.py
@@ -0,0 +1,10 @@
+class car:
+    @staticmethod
+    def car_info():
+        print("This is a car class")
+    @staticmethod
+    def car_speed():
+        print("This car is very fast")  
+car.car_info()
+car.car_speed()
+    
